3c2ab4d5909dcf3d63d7aff5861948b8df587928
Aoneo <Aoneo@users.noreply.github.com>
2017-12-04 20:08:42 +0200

    Update responsiveness
    Please keep working on making it responsive

---
+++ res/js/index.js
@@ -0,0 +1,9 @@
+// const h1 = document.getElementById('template');
+//
+// function setScreenSize() {
+//   h1.innerHTML = window.screen.availWidth + ', ' + window.screen.availHeight;
+// }
+//
+// setScreenSize();
+//
+// window.addEventListner(resize, setScreenSize());
